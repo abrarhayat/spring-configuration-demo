04dff76e209b4b314092191d34933ac0434e3f44
Abrar Hayat <abrarhayat@gmail.com>
2020-04-26 01:47:32 +0600

    Added a custom factory object and injected it to HibernateSpeakerRepo by autowiring with a bean definition in app config

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,16 +1,21 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 @Scope(value = BeanDefinition.SCOPE_SINGLETON)
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     @Override
     public List<Speaker> findAll() {
@@ -20,6 +25,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Abrar");
         speaker.setLastName("Hayat");
+
+        System.out.println("cal: " + cal.getTime());
 
         speakers.add(speaker);
 
